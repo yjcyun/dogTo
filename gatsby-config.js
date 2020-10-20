e1100dcf873c7653ee825d050607e258064e5e09
Christina Yujeong Yun <yjc.yun@gmail.com>
2020-10-20 18:50:45 -0400

    Initial setup

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,10 +1,15 @@
-/**
- * Configure your Gatsby site with this file.
- *
- * See: https://www.gatsbyjs.com/docs/gatsby-config/
- */
+const path = require(`path`)
 
 module.exports = {
-  /* Your site config here */
-  plugins: [],
+  plugins: [
+    `gatsby-plugin-sharp`,
+    `gatsby-transformer-sharp`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: path.join(__dirname, `src`, `images`),
+      },
+    }
+  ],
 }
