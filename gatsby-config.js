c3b9fdf3c3d2a63e08d597acdc3ca59e4774aa0b
Christina Yujeong Yun <yjc.yun@gmail.com>
2020-10-22 14:00:17 -0400

    Move mdx folder to root folder- testing1

--- gatsby-config.js
+++ gatsby-config.js
@@ -16,7 +16,7 @@ module.exports = {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `articles`,
-        path: `${__dirname}/src/articles`
+        path: `${__dirname}/articles`
       },
     },
     {
